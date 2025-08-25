aa7fab0bf424875b8b5ab0646dc4f1db07d48d1f
Mustafa_Barakat <mustafabarakat633@gmail.com>
2025-08-25 23:46:09 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
-import { SkillsSection } from "../components/SkillsSection";
+// import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
@@ -22,7 +22,7 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
-        <SkillsSection />
+        {/* <SkillsSection /> */}
         <ProjectsSection />
         <ContactSection />
       </main>
